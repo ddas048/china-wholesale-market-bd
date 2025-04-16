6f08787138b5a6fb3882a26b3e1ef504224ee55b
Soumitra <71151027+Soumitraag7@users.noreply.github.com>
2025-04-16 19:18:49 +0600

    update: home product section

--- components/home-product.tsx
+++ components/home-product.tsx
@@ -2,8 +2,14 @@ import { ProductCard } from "./product-card";
 
 export default function HomeProduct() {
   return (
-    <div>
-      <ProductCard />
+    <div className="flex flex-col items-center pt-14">
+      <p className="w-full text-left text-2xl font-medium">Popular products</p>
+      <div className="mt-6 grid w-full grid-cols-2 flex-col items-center gap-6 pb-14 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <ProductCard />
+      </div>
+      <button className="rounded border px-12 py-2.5 text-gray-500/70 transition hover:bg-slate-50/90">
+        See more
+      </button>
     </div>
   );
 }
