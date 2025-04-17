b152893ff73fda63e0c52003c86dba41e725670c
Soumitra <71151027+Soumitraag7@users.noreply.github.com>
2025-04-17 18:27:43 +0600

    update: featured-product and banner component

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,3 +1,43 @@
+import { assets } from "@/lib/assets";
+import Image from "next/image";
+
 export default function Banner() {
-  return <div>Banner</div>;
+  return (
+    <div className="my-16 flex flex-col items-center justify-between overflow-hidden rounded-xl bg-[#E6E9F2] py-14 md:flex-row md:py-0 md:pl-20">
+      <Image
+        className="max-w-56"
+        src={assets.jbl_soundbox_image}
+        alt="jbl_soundbox_image"
+      />
+
+      <div className="flex flex-col items-center justify-center space-y-2 px-4 text-center md:px-0">
+        <h2 className="max-w-[290px] text-2xl font-semibold md:text-3xl">
+          Level Up Your Gaming Experience
+        </h2>
+        <p className="max-w-[343px] font-medium text-gray-800/60">
+          From immersive sound to precise controls—everything you need to win
+        </p>
+        <button className="group flex items-center justify-center gap-1 rounded bg-orange-600 px-12 py-2.5 text-white">
+          Buy now
+          <Image
+            className="transition group-hover:translate-x-1"
+            src={assets.arrow_icon_white}
+            alt="arrow_icon_white"
+          />
+        </button>
+      </div>
+
+      <Image
+        className="hidden max-w-80 md:block"
+        src={assets.md_controller_image}
+        alt="md_controller_image"
+      />
+
+      <Image
+        className="md:hidden"
+        src={assets.sm_controller_image}
+        alt="sm_controller_image"
+      />
+    </div>
+  );
 }
--- components/featured-product.tsx
+++ components/featured-product.tsx
@@ -1,3 +1,64 @@
+import { assets } from "@/lib/assets";
+import Image from "next/image";
+
+const products = [
+  {
+    id: 1,
+    image: assets.girl_with_headphone_image,
+    title: "Unparalleled Sound",
+    description: "Experience crystal-clear audio with premium headphones.",
+  },
+  {
+    id: 2,
+    image: assets.girl_with_earphone_image,
+    title: "Stay Connected",
+    description: "Compact and stylish earphones for every occasion.",
+  },
+  {
+    id: 3,
+    image: assets.boy_with_laptop_image,
+    title: "Power in Every Pixel",
+    description: "Shop the latest laptops for work, gaming, and more.",
+  },
+];
+
 export default function FeaturedProduct() {
-  return <div>FeaturedProducts</div>;
+  return (
+    <div className="mt-14">
+      <div className="flex flex-col items-center">
+        <p className="text-3xl font-medium">Featured Products</p>
+        <div className="mt-2 h-0.5 w-28 bg-orange-600"></div>
+      </div>
+
+      <div className="mt-12 grid grid-cols-1 gap-8 px-4 sm:grid-cols-2 md:px-14 lg:grid-cols-3 lg:gap-14">
+        {products.map(({ id, image, title, description }) => (
+          <div key={id} className="group relative">
+            <Image
+              src={image}
+              alt={title}
+              width={500}
+              height={500}
+              className="h-auto w-full object-cover transition duration-300 group-hover:brightness-75"
+            />
+            <div className="absolute bottom-8 left-8 space-y-2 text-white transition duration-300 group-hover:-translate-y-4">
+              <p className="text-xl font-medium lg:text-2xl">{title}</p>
+              <p className="max-w-60 text-sm leading-5 lg:text-base">
+                {description}
+              </p>
+              <button className="flex items-center gap-1.5 rounded bg-orange-600 px-4 py-2">
+                Buy now{" "}
+                <Image
+                  className="h-3 w-3"
+                  src="/assets/redirect_icon.svg"
+                  alt="Redirect Icon"
+                  width={500}
+                  height={500}
+                />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
