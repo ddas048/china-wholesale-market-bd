b61d45c5f53b447663915427560af28104e21c61
Soumitra <71151027+Soumitraag7@users.noreply.github.com>
2025-04-17 18:45:23 +0600

    update: home-product

--- components/home-product.tsx
+++ components/home-product.tsx
@@ -5,13 +5,13 @@ export default function HomeProduct() {
   return (
     <div className="flex flex-col items-center pt-14">
       <p className="w-full text-left text-2xl font-medium">Popular products</p>
-      <div className="mt-6 grid w-full grid-cols-2 flex-col items-center gap-6 pb-14 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-        {/* <ProductCard /> */}
 
+      <div className="mt-6 grid w-full grid-cols-2 flex-col items-center gap-6 pb-14 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {products.map((product, index) => (
           <ProductCard key={index} product={product} />
         ))}
       </div>
+
       <button className="rounded border px-12 py-2.5 text-gray-500/70 transition hover:bg-slate-50/90">
         See more
       </button>
